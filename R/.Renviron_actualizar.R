3f28f150e63aae409d9370be0c13e49203d1ed99
rubenp <ruben.perper@gmail.com>
2025-12-19 10:26:01 +0100

    refactor (repo structure): change folders names and location to adapt to future R package

--- R/.Renviron_actualizar.R
+++ R/.Renviron_actualizar.R
@@ -7,7 +7,7 @@ usethis::edit_r_environ("project")
 
 # 2. Modificar documento que aparece tras la última línea
 
-# 3. Guardar documento
+# 3. Guardar documento. Correr desde la consola con .Renviron abierto
 rstudioapi::documentSave()
 
 # 4. Hacer efectivos los cambios reiniciando la sesion de R
